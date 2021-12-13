655c81f0c6e1511c2959a375feacb2cae211312d
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2021-12-13 16:23:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
     function apiGet($path) {
-        $url = 'localhost/api/';
+        $url = 'http://10.123.45.2/api/';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url . $path); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
@@ -17,8 +20,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="styleshe>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integri>
     <title>5Mensche</title>
 </head>
 <body style="height: 100vh;">
