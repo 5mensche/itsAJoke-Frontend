4fc3508249f4cf3152d1fcfe9a89b2e6aa0b8085
HATBE2113 <64154447+HATBE@users.noreply.github.com>
2021-12-14 10:43:27 +0100

    Adding first version of like system

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <?php
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    $apiUrl = 'http://10.123.45.2/api/';
+
     function apiGet($path) {
-        $url = 'http://10.123.45.2/api/';
+        global $apiUrl;
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url . $path); 
+        curl_setopt($ch, CURLOPT_URL, $apiUrl . $path); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        $output = curl_exec($ch); 
+        $output = curl_exec($ch);
         curl_close($ch);
         return json_decode($output, true);;
     }
+    function apiPost($path, $payload) {
+        global $apiUrl;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $apiUrl . $path);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result, true);
+    }
 
     $joke = apiGet('joke');
+
+    #echo apiPost('rate', 'rating=0&id=1')['type'];
+    if(isset($_GET['rating']) && isset($_GET['jokeId'])) {
+        if(!empty($_GET['rating']) 
+        && is_numeric($_GET['rating']) 
+        && !empty($_GET['jokeId'])
+        && is_numeric($_GET['jokeId'])) {
+            apiPost('rate', 'rating=' . $_GET['rating'] . '&jokeId=' . $_GET['jokeId'])['type'];
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,13 +44,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="styleshe>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integri>
-    <title>5Mensche</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonym">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>ItsAJoke | 5 Mensche</title>
 </head>
 <body style="height: 100vh;">
     <div style="height: 100%;" class="d-flex justify-content-center align-items-center">
-        <h5><?=$joke['message'];?></h5>
+        <div>
+            <h5 class="d-block"><?=$joke['message'];?></h5>
+            <h6><a class="d-block" href="?rating=1&jokeId=2">Like</a><a href="?rating=2&jokeId=1">Dislike</a></h6>
+        </div>
     </div>
 </body>
 </html>
