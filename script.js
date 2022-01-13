6244e3c8d9213ab66a4efe273aea7f2c2fca3898
HATBE <admin@aarongensetter.ch>
2022-01-13 12:16:59 +0100

    refactor code

--- script.js
+++ script.js
@@ -1,78 +1,116 @@
-let apiUrl = "/api/";
-let jokeId = null;
+'use strict';
+
+let apiUrl = 'https://itsajoke.5mensche.ch/api/';
+let currentJokeId = null;
+
+let btnRefresh = document.querySelector('#btn-refresh');
+let btnLike = document.querySelector('#btn-like');
+let btnDislike = document.querySelector('#btn-dislike');
+
+let jokeEL = document.querySelector('#joke');
+let ratingEL = document.querySelector('#rating');
+let top5El = document.querySelector('#top5');
 let toast;
-let oldJokeId = null;
-loadNewJoke();
-function loadNewJoke(showMsg = false) {
-    loadTop5();
-    $.get(apiUrl + "jokes/random", function (data) {
-        if (data == null || data.status != 200) {
-            showToast("Loading joke", "Error", "Error while loading new Joke!", "bg-danger");
-        } else {
-            jokeId = data.id;
-            if (jokeId == oldJokeId) {
-                loadNewJoke();
-            } else {
-                oldJokeId = jokeId;
-                document.getElementById("joke").innerHTML = data.joke;
-                document.getElementById("rating").innerHTML = "Rating: <span class='badge badge-pill text-light bg-primary'>" + data.rating + "</span>";
-                if (showMsg) {
-                    showToast("Loading joke", "Success", "successfully loaded new joke.", "bg-success");
-                }
-            }
-        }
-    }).fail(function () {
-        showToast("Loading joke", "Error", "Error while loading new Joke!", "bg-danger");
-    });
+
+let currentTaskComplete = true;
+
+function init() {
+    createTop5();
+    refresh(false);
 }
-function loadTop5() {
-    $.get(apiUrl + "jokes/listtop5", function (data) {
-        if (data == null || data.status != 200) {
-            showToast("Loading joke", "Error", "Error while loading new Joke!", "bg-danger");
-        } else {
-            let c = 1;
-            let top5 = document.getElementById('top5');
-            top5.innerHTML = '';
-            data.jokes.forEach(joke => {
-                let el = document.createElement("div");
-                el.innerHTML = "<span class='text-light'>" + c + ". <span class='badge badge-pill bg-primary text-light'>" + joke.rating + "</span> " + joke.joke + "</span>";
-                el.classList.add('py-3')
-                top5.appendChild(el);
-                c++;
-            });
-        }
-    }).fail(function () {
-        showToast("Loading joke", "Error", "Error while loading new top Jokes!", "bg-danger");
-    });
-}
-function rate(id, rating) {
-    if (id != null) {
-        $.post(apiUrl + "jokes/rate", { id: id, rating: rating })
-            .done(function (data) {
-                if (data.status == 200) {
-                    if (rating == 2) {
-                        showToast("Dislike", "Success", "Disliked successfully", "bg-success");
-                    } else {
-                        showToast("Like", "Success", "Liked successfully", "bg-success");
-                    }
-                    loadNewJoke();
-                } else {
-                    showToast("Rating joke", "Error", "Error while rating Joke!", "bg-danger");
-                }
-            })
-            .fail(function () {
-                showToast("Loading joke", "Error", "Error while rating Joke!", "bg-danger");
-            });
-    } else {
-        showToast("Loading joke", "Error", "Error while rating Joke!", "bg-danger");
+
+function refresh(showMsg = true) {
+    if(currentTaskComplete) {
+        getRandomJoke(showMsg);
+        getTop5();
     }
 }
-function like() {
-    rate(jokeId, 1);
+
+function rate(id, rating) {
+    if (id != null || rating != null) {
+        $.post(apiUrl + 'jokes/rate', {id: id, rating: rating})
+        .done((data) => {
+            if(data == null || data.status != 200) {
+                showToast("Rating joke", "Error", "Error while rating Joke!", "bg-danger");
+                return;
+            }
+            if (rating == 1) {
+                showToast("Like", "Success", "Liked successfully", "bg-success");
+            } else {
+                showToast("Dislike", "Success", "Disliked successfully", "bg-success");
+            }
+            refresh(false);
+        });
+    }
 }
-function dislike() {
-    rate(jokeId, 2);
+
+function createTop5() {
+    for (let i = 1; i <= 5; i++) {
+        let div = document.createElement('div');
+        div.classList.add('py-3')
+
+        let place = document.createElement('span');
+        place.id = `top--${i}--place`;
+        place.textContent = `${i}.`;
+
+        let rating = document.createElement('span');
+        rating.id = `top--${i}--rating`;
+        rating.classList.add('badge', 'badge-pill', 'bg-primary', 'mx-1');
+        rating.textContent = '0';
+
+        let joke = document.createElement('span');
+        joke.id = `top--${i}--joke`;
+
+        div.append(place);
+        div.append(rating);
+        div.append(joke);
+
+        top5El.appendChild(div);
+    }
 }
+
+function getRandomJoke(showMsg = false) {
+    currentTaskComplete = false;
+    $.get(apiUrl + 'jokes/random', (data) => {
+        if(data == null || data.status != 200) {
+            showToast("Loading joke", "Error", "Error while loading new Joke!", "bg-danger");
+            return;
+        }
+
+        if(showMsg) {
+            showToast("Loading joke", "Success", "successfully loaded new joke.", "bg-success");
+        }
+        currentTaskComplete = true;
+
+        currentJokeId = data.id;
+        jokeEL.textContent = data.joke;
+        ratingEL.textContent = data.rating;
+    });
+}
+
+function getTop5() {
+    currentTaskComplete = false;
+    $.get(apiUrl + 'jokes/listtop5', (data) => {
+        if(data == null || data.status != 200) {
+            showToast("Loading joke", "Error", "Error while loading new top Jokes!", "bg-danger");
+            return;
+        }
+
+        currentTaskComplete = true;
+
+        let c = 1;
+        data.jokes.forEach(joke => {
+            let t5RatingEl = document.querySelector(`#top--${c}--rating`);
+            let t5JokeEl = document.querySelector(`#top--${c}--joke`);
+
+            t5RatingEl.textContent = joke.rating;
+            t5JokeEl.textContent = joke.joke;
+
+            c++;
+        });
+    });
+}
+
 function showToast(title, status, message, cclass) {
     document.getElementById("toastHeader").classList.remove("bg-success");
     document.getElementById("toastHeader").classList.remove("bg-danger");
@@ -82,3 +120,9 @@ function showToast(title, status, message, cclass) {
     document.getElementById("toastHeader").classList.add(cclass);
     toast = new bootstrap.Toast(document.getElementById("liveToast")).show();
 }
+
+btnRefresh.addEventListener('click', refresh);
+btnLike.addEventListener('click', () => {rate(currentJokeId, 1)});
+btnDislike.addEventListener('click',  () => {rate(currentJokeId, 2)});
+
+init();
